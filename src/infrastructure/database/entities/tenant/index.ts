462d347cd81dec77ee231e1bfdb71c000da227cb
Andrey Kaiky Reis Ferreira <Andrey.kaiky@outlook.com>
2025-10-29 23:35:30 -0300

    chore: Retirando entidades do database

--- src/infrastructure/database/entities/tenant/index.ts
+++
@@ -1,5 +0,0 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, OneToMany } from 'typeorm';
-import { User } from '@modules/users/entities/user.entity';
-
-@Entity('tenants')
-export class Tenant { }
--- src/infrastructure/database/entities/user/index.ts
+++
@@ -1,5 +0,0 @@
-import { Column, CreateDateColumn, Entity, Index, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
-import { Tenant } from "@database/entities/tenant";
-
-@Entity()
-export class Users {}
