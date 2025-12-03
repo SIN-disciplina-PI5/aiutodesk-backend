883c7f2c9bc310a6df25011ca0dc5e428f0836c9
guicms03 <144068360+guicms03@users.noreply.github.com>
2025-12-03 03:35:07 +0000

    CRUD  CATEGORIAS

--- src/modules/knowledge/knowledge.module.ts
+++ src/modules/knowledge/knowledge.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ArticlesService } from './articles/articles.service';
 import { CategoriesService } from './categories/categories.service';
+import { CategoriesModule } from './categories/categories.module'
 
 @Module({
+  imports: [CategoriesModule],
   providers: [ArticlesService, CategoriesService],
 })
 export class KnowledgeModule {}
