20a14716ff09f8daedd8291308818e4a301f58e7
Alyson Lima <alysonrafael2005@gmail.com>
2025-12-03 01:36:48 +0000

    CRUD de Artigos

--- src/modules/knowledge/knowledge.module.ts
+++ src/modules/knowledge/knowledge.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ArticlesService } from './articles/articles.service';
 import { CategoriesService } from './categories/categories.service';
+import { ArticlesModule } from './articles/articles.module';
 
 @Module({
+  imports: [ArticlesModule],
   providers: [ArticlesService, CategoriesService],
 })
+
 export class KnowledgeModule {}
