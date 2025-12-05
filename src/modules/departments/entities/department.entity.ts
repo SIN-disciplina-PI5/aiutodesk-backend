ffef0d0ebca810d4f63d316c83104c90f40e5217
Daniel Paiva <danielpaiva1418@gmail.com>
2025-12-04 23:37:01 -0300

    teAmoMarcio

--- src/modules/departments/entities/department.entity.ts
+++ src/modules/departments/entities/department.entity.ts
@@ -1,1 +1,38 @@
-export class Department {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  RelationId,
+  UpdateDateColumn
+} from "typeorm";
+import { Tenant } from "@modules/tenants/entities/tenant.entity";
+
+@Entity('departments')
+export class Department {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+
+  @Column({ name: 'cost_center', type: 'varchar', length: 255, nullable: true })
+  costCenter: string;
+
+  @ManyToOne(() => Tenant, { nullable: false, onDelete: 'RESTRICT' })
+  @JoinColumn({ name: 'tenant_id' })
+  tenant: Tenant;
+
+  @RelationId((department: Department) => department.tenant)
+  tenantId: string;
+
+  @Index()
+  @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at', type: 'timestamp' })
+  updatedAt: Date;
+}
